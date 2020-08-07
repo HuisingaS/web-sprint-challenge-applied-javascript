017692b5a34965720d6531493016d223435a9846
Stephen Huisinga <stephenhuisinga@gmail.com>
2020-08-07 12:49:59 -0500

    end sprint part 1...

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,13 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+// const cardContainer = document.querySelector(".cards-container");
+
+
+//     //axios data request
+// axios.get("https://lambda-times-api.herokuapp.com/articles")
+// .then(function(response) {
+//     console.log(response);
+
+// const 
